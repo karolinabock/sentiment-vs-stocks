1c23459e208de2e646e2e06e4b5a9678849b6df4
unknown <bockkarolina@gmail.com>
2025-05-22 18:04:26 +0200

    2.1 perfecto

--- src/etl/fetch_news.py
+++ src/etl/fetch_news.py
@@ -16,7 +16,7 @@ cutoff = now.replace(hour=0, minute=0, second=0, microsecond=0)
 
 # browser config
 options = Options()
-#options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--start-maximized")
 driver = webdriver.Chrome(options=options)
 actions = ActionChains(driver)
@@ -62,68 +62,60 @@ previous_items_count = 0
 # datalist
 data = []
 
-MIN_NEWS = 200
-MAX_ATTEMPTS = 2
-attempt = 0
+while scroll_count < MAX_SCROLLS:
+    scroll_count += 1
+    print(f"\n Page Down {scroll_count}/{MAX_SCROLLS}")
 
-while len(data) < MIN_NEWS and attempt < MAX_ATTEMPTS:
-    attempt += 1
-    print("Second attempt")
+    body.send_keys(Keys.PAGE_DOWN)
+    time.sleep(SCROLL_PAUSE)
 
-    while scroll_count < MAX_SCROLLS:
-        scroll_count += 1
-        print(f"\n Page Down {scroll_count}/{MAX_SCROLLS}")
-
-        body.send_keys(Keys.PAGE_DOWN)
+    try:   # load more button finder 
+        load_more_button = WebDriverWait(driver, 3).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "button-loadmore--more"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView();", load_more_button)
+        time.sleep(1)
+        driver.execute_script("arguments[0].click();", load_more_button)
         time.sleep(SCROLL_PAUSE)
+    except Exception:
+        pass
 
-        try:   # load more button finder 
-            load_more_button = WebDriverWait(driver, 3).until(
-                EC.element_to_be_clickable((By.CLASS_NAME, "button-loadmore--more"))
-            )
-            driver.execute_script("arguments[0].scrollIntoView();", load_more_button)
-            time.sleep(1)
-            driver.execute_script("arguments[0].click();", load_more_button)
-            time.sleep(SCROLL_PAUSE)
-        except Exception:
-            pass
+    news_items = driver.find_elements(By.CLASS_NAME, "flash-item")
+    current_items_count = len(news_items)
 
-        news_items = driver.find_elements(By.CLASS_NAME, "flash-item")
-        current_items_count = len(news_items)
+    # stuck checker
+    if current_items_count == previous_items_count:
+        stuck_scrolls += 1
+        if stuck_scrolls >= MAX_STUCK_SCROLLS:
+            break
+    else:
+        stuck_scrolls = 0
+    previous_items_count = current_items_count
+        #only new data
+    for item in news_items[len(data):]:
+        try:
+            time_elem = item.find_elements(By.TAG_NAME, "span")
+            raw_time = time_elem[0].text.strip() if time_elem else None
 
-        # stuck checker
-        if current_items_count == previous_items_count:
-            stuck_scrolls += 1
-            if stuck_scrolls >= MAX_STUCK_SCROLLS:
-                break
-        else:
-            stuck_scrolls = 0
-        previous_items_count = current_items_count
-            #only new data
-        for item in news_items[len(data):]:
-            try:
-                time_elem = item.find_elements(By.TAG_NAME, "span")
-                raw_time = time_elem[0].text.strip() if time_elem else None
-
-                content_elem = item.find_elements(By.CLASS_NAME, "flash-content")
-                content = content_elem[0].text.strip() if content_elem else None
-                #data parsing
-                if raw_time and content:
-                    news_time = datetime.combine(now.date(), datetime.strptime(raw_time, "%H:%M:%S").time())
-                    if news_time > now:
-                        news_time -= timedelta(days=1)
-                #cutoff
-                    if news_time >= cutoff:
-                        data.append({
-                            "date": news_time.strftime("%Y-%m-%d %H:%M:%S"),
-                            "content": content
-                        })
-                    else:
-                        print("Cutoff time found.")
-                        scroll_count = MAX_SCROLLS
-                        break
-            except Exception as e:
-                print(f"Parsing Error {e}")
+            content_elem = item.find_elements(By.CLASS_NAME, "flash-content")
+            content = content_elem[0].text.strip() if content_elem else None
+            #data parsing
+            if raw_time and content:
+                news_time = datetime.combine(now.date(), datetime.strptime(raw_time, "%H:%M:%S").time())
+                if news_time > now:
+                    news_time -= timedelta(days=1)
+            #cutoff
+                if news_time >= cutoff:
+                    data.append({
+                        "date": news_time.strftime("%Y-%m-%d %H:%M:%S"),
+                        "content": content
+                    })
+                else:
+                    print("Cutoff time found.")
+                    scroll_count = MAX_SCROLLS
+                    break
+        except Exception as e:
+            print(f"Parsing Error {e}")
 
 # CSV write
 df = pd.DataFrame(data)
